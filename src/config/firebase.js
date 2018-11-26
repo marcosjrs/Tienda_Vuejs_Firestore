1872d026bbe0875c2df462c111c10c9f0a3b198c
marcosjrs <marcosjrs80@gmail.com>
2018-11-26 20:32:28 +0100

    Configuración de Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+// Exporta la configuración de firebase.
+//Se obtuvo creando el proyecto y lugo pulsando en el botón "</>", que al pulsarlo
+//muestra un popup con titulo "Añade Firebase a tu aplicación web"
+//En nuestro caso tambien :
+//       Habilitamos en   Authentication -> "Correo electrónico/contraseña"
+//       Creamos BBDD en    Database -> "Crear base de datos" (Cloud Firestore) -> Opcion "Empezar con el modo de bloqueo"
+
+export default {
+    apiKey: "AIzaSyDKMv7jJ4Dq2PiSQ8QY_bU92-nMNPNFSy0",
+    authDomain: "cursovuefirestore-mjrs.firebaseapp.com",
+    databaseURL: "https://cursovuefirestore-mjrs.firebaseio.com",
+    projectId: "cursovuefirestore-mjrs",
+    storageBucket: "cursovuefirestore-mjrs.appspot.com",
+    messagingSenderId: "1014744825660"
+};
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//firebase
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from '@/config/firebase'
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore(); //db.collections... db.doc.... 
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
