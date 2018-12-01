70ccff2231e4872e75a85c03c066db597375f1e3
marcosjrs <marcosjrs80@gmail.com>
2018-12-01 11:51:33 +0100

    Componente Auth. Inicio Componente Registro (usando Compo Auth)

---
+++ src/components/forms/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+
+    <auth-form
+      action="register"
+      v-on:process="register($event)"
+    />
+
+    <app-snack-bar
+      v-if="snackBar"
+      :snack-bar="snackBar"
+      :text="message"
+      :timeout="timeout"
+    />
+  </div>
+</template>
+
+<script>
+	import AppSnackBar from "@/components/snackbar/Snackbar";
+    import AuthForm from "@/components/forms/Auth";
+    import {db} from '@/main';
+    export default {
+        name: "Register",
+        components: {AuthForm, AppSnackBar},
+        data () {
+            return {
+                snackBar: false,
+                message: '',
+                timeout: 5000
+            }
+        },
+        methods: {
+            register (user) {
+                //despatchará un acción del store para que registre el usuario, y se actualizará el user y el tipo de usuario (rol)
+                console.log(user);
+            }
+        }
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Register from '@/components/forms/Register'
 
 Vue.use(Router)
 
@@ -12,6 +13,14 @@ export default new Router({
       component: Home,
       meta:{
         Auth: false, title: 'Inicio' //En el futuro se utilizarán estos datos extras..
+      }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta:{
+        Auth: false, title: 'Registro' //En el futuro se utilizarán estos datos extras..
       }
     }
   ]
